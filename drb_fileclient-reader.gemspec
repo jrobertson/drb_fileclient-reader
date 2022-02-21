4ea88b07052f7d74378098bdbcb12f583f2bcb86
James Robertson <digital.robertson@gmail.com>
2022-02-21 23:55:34 +0000

    1st commit

---
+++ drb_fileclient-reader.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'drb_fileclient-reader'
+  s.version = '0.1.0'
+  s.summary = 'A DRb file reader client to access the DRb_fileserver service.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/drb_fileclient-reader.rb']
+  s.add_runtime_dependency('c32', '~> 0.3', '>=0.3.0')
+  s.signing_key = '../privatekeys/drb_fileclient-reader.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'digital.robertson@gmail.com'
+  s.homepage = 'https://github.com/jrobertson/drb_fileclient-reader'
+end
---
+++ lib/drb_fileclient-reader.rb
@@ -0,0 +1,82 @@
+#!/usr/bin/env ruby
+
+# file: drb_fileclient-reader.rb
+
+require 'drb'
+require 'c32'
+
+
+class DRbFileClientReader
+  using ColouredText
+
+  def initialize(location=nil, host: nil, port: '61010', debug: false)
+    
+    @debug = debug
+    
+    if location then
+      
+      host = location[/(?<=^dfs:\/\/)[^\/:]+/]
+      port = location[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
+      @directory = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
+      
+    end
+    
+    DRb.start_service
+    
+  end
+  
+  def exists?(filename=@filename)  
+    
+    return File.exists? filename unless @directory or filename =~ /^dfs:\/\//
+    
+    if filename =~ /^dfs:\/\// then
+      @file, filename2 = parse_path(filename)
+    else
+
+      filename2 = File.join(@directory, filename)
+    end
+
+    @file.exists?(filename2)
+    
+  end
+  
+  alias exist? exists?
+
+  def read(filename=@filename)
+    
+    return File.read filename, s unless @directory or filename =~ /^dfs:\/\//
+    
+    if filename =~ /^dfs:\/\// then
+      @file, path = parse_path(filename)
+    else
+      path = File.join(@directory, filename)
+    end
+    
+    @file.read path
+  end
+  
+  protected
+  
+  def parse_path(filename)
+
+    host = filename[/(?<=^dfs:\/\/)[^\/:]+/]
+    @host = host if host
+    
+    port = filename[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
+
+    file_server = DRbObject.new nil, "druby://#{host || @host}:#{port}"
+    [file_server, filename[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1], ("dfs://%s:%s" % [host, port])]
+
+  end
+
+end 
+
+class DfsFile
+  
+  @client = DRbFileClientReader.new
+  
+  def self.exists?(filename)    @client.exists?(filename)    end  
+  def self.read(filename)       @client.read(filename)       end
+  
+end
+
