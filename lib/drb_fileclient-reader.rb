5b0f26787c63a69b89844f3e334b99cccbd78319
James Robertson <digital.robertson@gmail.com>
2022-02-23 18:44:09 +0000

    bug fix: instantce variables replaced with class variables to allow file operations on relative paths.

--- lib/drb_fileclient-reader.rb
+++ lib/drb_fileclient-reader.rb
@@ -10,58 +10,58 @@ class DRbFileClientReader
   using ColouredText
 
   def initialize(location=nil, host: nil, port: '61010', debug: false)
-    
+
     @debug = debug
-    
+
     if location then
-      
+
       host = location[/(?<=^dfs:\/\/)[^\/:]+/]
       port = location[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
-      @directory = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
-      
+      @@directory = location[/(?<=^dfs:\/\/)[^\/]+\/(.*)/,1]
+
     end
-    
+
     DRb.start_service
-    
+
   end
-  
-  def exists?(filename=@filename)  
-    
-    return File.exists? filename unless @directory or filename =~ /^dfs:\/\//
-    
+
+  def exists?(filename=@@filename)
+
+    return File.exists? filename unless @@directory or filename =~ /^dfs:\/\//
+
     if filename =~ /^dfs:\/\// then
-      @file, filename2 = parse_path(filename)
+      @@file, filename2 = parse_path(filename)
     else
 
-      filename2 = File.join(@directory, filename)
+      filename2 = File.join(@@directory, filename)
     end
 
-    @file.exists?(filename2)
-    
+    @@file.exists?(filename2)
+
   end
-  
+
   alias exist? exists?
 
-  def read(filename=@filename)
-    
-    return File.read filename, s unless @directory or filename =~ /^dfs:\/\//
-    
+  def read(filename=@@filename)
+
+    return File.read filename, s unless @@directory or filename =~ /^dfs:\/\//
+
     if filename =~ /^dfs:\/\// then
-      @file, path = parse_path(filename)
+      @@file, path = parse_path(filename)
     else
-      path = File.join(@directory, filename)
+      path = File.join(@@directory, filename)
     end
-    
-    @file.read path
+
+    @@file.read path
   end
-  
+
   protected
-  
+
   def parse_path(filename)
 
     host = filename[/(?<=^dfs:\/\/)[^\/:]+/]
     @host = host if host
-    
+
     port = filename[/(?<=^dfs:\/\/)[^:]+:(\d+)/,1]  || '61010'
 
     file_server = DRbObject.new nil, "druby://#{host || @host}:#{port}"
@@ -69,14 +69,13 @@ class DRbFileClientReader
 
   end
 
-end 
-
-class DfsFile
-  
-  @client = DRbFileClientReader.new
-  
-  def self.exists?(filename)    @client.exists?(filename)    end  
-  def self.read(filename)       @client.read(filename)       end
-  
 end
 
+class DfsFile
+
+  @client = DRbFileClientReader.new
+
+  def self.exists?(filename)    @client.exists?(filename)    end
+  def self.read(filename)       @client.read(filename)       end
+
+end
